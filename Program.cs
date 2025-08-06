603418dd14acad0e4496b81776ab9241b690b548
sefad <sefad@LAPTOP-6GMKGUFJ>
2025-08-06 09:28:56 +0300

    main: commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
